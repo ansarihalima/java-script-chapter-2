7247c5465ec9d517f2eb2c1e5c5b84dc2d24e017
ansarihalima <halimasadiahalimasadia00@gmail.com>
2024-05-22 03:40:01 -0700

    second chapter

---
+++ index.js
@@ -0,0 +1,88 @@
+///////////////QUESTION-1
+
+// Declare a variable called username
+var username;
+
+///////////////QUESTION-2
+
+// Declare a variable called myName and assign a string representing your full name
+var myName = "Ansari Halima";
+
+
+/////////////QUESION-3
+
+
+// Step a: Declare a JS variable titled message
+var message;
+
+// Step b: Assign "Hello World" to variable message
+message = "Hello World";
+
+// Step c: Display the message in an alert box
+alert(message);
+
+/////////////QUESION-4
+
+// Define variables to store student's bio data
+var studentName = "Johne Doe";
+var age = 20;
+var gender = "Male";
+var grade = "A+";
+
+// Construct the message to display in the alert box
+var message = "Student's Bio Data:\n";
+message += "Name: " + studentName + "\n";
+message += "Age: " + age + "\n";
+message += "Gender: " + gender + "\n";
+message += "Grade: " + grade;
+
+// Display the student's bio data in an alert box
+alert(message);
+
+/////////////QUESION-5
+
+// Define a variable to store the text
+var message = "pizza\npizz\npiz\npi\np";
+
+// Display the alert with the message
+alert(message);
+
+/////////////QUESION-6
+// Declare a variable called email and assign it a string representing your email address
+var email = "example@example.com";
+
+// Construct the message to display in the alert box
+var message = "Your email address is: " + email;
+
+// Display the message in an alert box
+alert(message);
+
+/////////////QUESION-7
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying too learn from a book: " + book);
+
+// Create a new <p> element
+var paragraph = document.createElement("p");
+
+// Set the text content of the paragraph
+paragraph.textContent = "yesh i am wite html through java script";
+
+// Append the paragraph to the body of the document
+document.body.appendChild(paragraph);
+
+
+/////////////QUESION-8
+
+// Store the string in a variable
+var string = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// Display in an alert
+alert(string);
+
+// Display in the browser
+document.write(string);
+
+
+
+
